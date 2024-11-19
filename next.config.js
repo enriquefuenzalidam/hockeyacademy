0db169d116f35079920ff388b056cf58a1d45b45
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2024-11-19 15:32:57 -0300

    next.config.js file update

--- next.config.js
+++ next.config.js
@@ -1,22 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.(mp4|webm|ogg|avi)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next/static/videos/',
-            outputPath: 'static/videos/',
-            name: '[name].[ext]',
-          },
-        },
-      });
+  reactStrictMode: true,
   
-      return config;
-    },
+  // Webpack custom configuration
+  webpack(config, options) {
+    // Disable caching temporarily
+    config.cache = false;
+    
+    // Existing file loader rules
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|swf|ogv)$/, // Include the file types you need
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/videos',
+          outputPath: 'static/videos',
+          name: '[name].[hash].[ext]',
+          esModule: false,
+        },
+      },
+    });
+
+    return config;
+  },
+  
+  // Redirect configuration
+  
 };
 
 module.exports = nextConfig;
